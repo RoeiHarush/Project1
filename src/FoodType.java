164b964492dcf43007a14b9799da1e393e75f11e
roeiHarush <roeiharush44@gmail.com>
2023-03-27 13:22:42 +0300

    Add Javadoc to Get mapping

---
+++ src/FoodType.java
@@ -0,0 +1,11 @@
+public enum FoodType {
+
+    BANANA("Banana"),
+    MEAT("Meat"),
+    FISH("Fish"),
+    BAMBA("Bamba");
+
+    FoodType(String name ) {this.name=name;}
+    final String name;
+
+    }
